d84385900d878c6ebd846a12b8d52ee4b444e15f
maeriil <jixanna@gmail.com>
2025-02-21 23:20:06 -0500

    jecs template using jabby and demo scheduler

---
+++ Packages/_Index/alicesaidhi_jabby@0.2.0-rc.9/jecs.lua
@@ -0,0 +1,10 @@
+local REQUIRED_MODULE = require(script.Parent.Parent["ukendio_jecs@0.5.3"]["jecs"])
+export type Archetype = REQUIRED_MODULE.Archetype 
+export type Record = REQUIRED_MODULE.Record 
+export type Id<T > =
+REQUIRED_MODULE.Id<T >
+export type Pair<P, O> = REQUIRED_MODULE.Pair<P, O>
+export type Entity<T > = REQUIRED_MODULE.Entity<T >
+export type Query<T...> = REQUIRED_MODULE.Query<T...>
+export type World = REQUIRED_MODULE.World 
+return REQUIRED_MODULE
---
+++ Packages/jabby.lua
@@ -0,0 +1,1 @@
+return require(script.Parent._Index["alicesaidhi_jabby@0.2.0-rc.9"]["jabby"])
